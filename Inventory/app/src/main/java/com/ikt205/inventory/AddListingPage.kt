763fa87991d9ffef15350b383d471493bd90567c
Simon Fladen Lie <simonfl@uia.no>
2021-03-24 21:27:28 +0100

    Added navigation to the addNewListing page

---
+++ Inventory/app/src/main/java/com/ikt205/inventory/AddListingPage.kt
@@ -0,0 +1,44 @@
+package com.ikt205.inventory
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.ikt205.inventory.databinding.FragmentAddListingPageBinding
+
+/**
+ * TODO: Change name of the activities to something more suitable
+ * TODO: Add some commenting
+ */
+class AddListingPage : Fragment() {
+
+    private var _binding: FragmentAddListingPageBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentAddListingPageBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Test to navigate back home
+        binding.goToNewListing.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
